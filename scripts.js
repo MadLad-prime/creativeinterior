f11c8b4b0d252496f6f6a3bf78f24b1810e132a0
MadLad-prime <azuresky01010@gmail.com>
2025-02-28 23:31:12 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,16 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("images.json")
         .then(response => response.json())
         .then(data => {
-            // Load Services
             let servicesList = document.getElementById("services-list");
-            servicesList.innerHTML = data.services.map(img => `
-                <li><img src="${img}" alt="Service Image"></li>
-            `).join("");
+            servicesList.innerHTML = data.services.map(img => `<li><img src="images/${img}" alt="Service"></li>`).join("");
 
-            // Load Projects
             let projectsList = document.getElementById("projects-list");
-            projectsList.innerHTML = data.projects.map(img => `
-                <div><img src="${img}" alt="Project Image"></div>
-            `).join("");
+            projectsList.innerHTML = data.projects.map(img => `<div><img src="images/${img}" alt="Project"></div>`).join("");
         });
 });
